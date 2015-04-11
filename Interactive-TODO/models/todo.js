7c17fccb6e8b936890e528c82636adebf1e230d7
shiv <shiv@192.168.0.103>
2015-04-11 14:46:56 +0530

    Updated the model according to the readme

--- Interactive-TODO/models/todo.js
+++ Interactive-TODO/models/todo.js
@@ -1,15 +1,16 @@
 // grab the mongoose module
 var mongoose = require('mongoose');
+var todoSchema = new mongoose.Schema({
+
+        name : String,
+        deadline : Date,
+        priority : Number,
+        group : String, //http://mongoosejs.com/docs/subdocs.html
+        subtodo : [String]
+});
+
 
 // define our todo model
 // module.exports allows us to pass this to other files when it is called
-var todoModel = mongoose.model('Todo', {
-    name: {
-        type: String
-    },
-    description: {
-        type: String
-    }
-});
-
+var todoModel = mongoose.model('Todo', todoSchema);
 module.exports = todoModel;
