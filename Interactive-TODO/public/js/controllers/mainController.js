e2fbee7eb30c5eb70e2c11927129afb644c65cee
shiv <shiv@192.168.0.103>
2015-04-18 13:04:37 +0530

    Changes to add as well as delete todo using the UI

--- Interactive-TODO/public/js/controllers/mainController.js
+++ Interactive-TODO/public/js/controllers/mainController.js
@@ -2,19 +2,50 @@ var todoApp = angular.module('todoApp', []);
 
 todoApp.controller('mainController', ['$scope', 'todoUIService', function($scope, todoUIService) {
 
+
+    var getAllTodo = function() {
         // Waiting for the services to return the data then setting the scope variables
         todoUIService.get().then(function(data) {
-        	console.log(data);
             $scope.todos = data.data;
+            if ($scope.todos !== null) {
+                $scope.selectedTodo = $scope.todos[0];
+            }
         });
+    };
+    getAllTodo();
 
-        $scope.populateTodo = function(id){
-        	var fetchedTodos = $scope.todos;
+    $scope.populateTodo = function(id) {
+        console.log("populate todo");
+        var fetchedTodos = $scope.todos;
 
-        	for ( todo in fetchedTodos ){
-        		if(fetchedTodos[todo]._id == id ){
-        		  	$scope.selectedTodo = fetchedTodos[todo];
-        	    }
-        	}
+        for (var todo in fetchedTodos) {
+            if (fetchedTodos[todo]._id == id) {
+                $scope.selectedTodo = fetchedTodos[todo];
+            }
         }
+    };
+
+    $scope.submitTodo = function() {
+        var obj = {};
+        obj.name = $scope.newTodo;
+        //create the new todo with the name that is provided
+        todoUIService.create(obj).then(function(data) {
+            //update the UI data and set the selelcted element to the new todo
+            if (data.data !== null) {
+                $scope.todos.push(data.data);
+                $scope.selectedTodo = data.data;
+            }
+        });
+        //clear the input text box once the todo is submitted
+        $scope.newTodo = '';
+    };
+
+    $scope.removeTodo = function(id) {
+        //delete the given id
+        todoUIService.delete(id).then(function(data) {
+            //update the UI data with the new todo's
+            getAllTodo();
+        });
+    };
+
 }]);
